d143522a896e9fc51e6b98f5774df341fc0bf27d
WilliamHaxhija <williamhaxhija@gmail.com>
2024-05-04 22:08:48 +0200

    feat: add bonus 2 and end project

--- index.php
+++ index.php
@@ -41,6 +41,7 @@ $hotels = [
 ];
 
 $parkFilter = isset($_GET['park-filter']) && $_GET['park-filter'] === 'on' ? true : false;
+$voteFilter = isset($_GET['vote']) ? intval($_GET['vote']) : 0;
 
 $filteredHotels = $hotels;
 
@@ -54,6 +55,15 @@ if ($parkFilter) {
     $filteredHotels = $hotelsWithParking;
 }
 
+if ($voteFilter > 0) {
+    $hotelsVoteFiltered = [];
+    foreach ($filteredHotels as $hotel) {
+        if ($hotel['vote'] >= $voteFilter) {
+            $hotelsVoteFiltered[] = $hotel;
+        }
+    }
+    $filteredHotels = $hotelsVoteFiltered;
+}
 
 ?>
 
@@ -75,9 +85,22 @@ if ($parkFilter) {
         <h1 class="mt text-center m-3">Hotels</h1>
 
         <form method="GET" class="ms-3">
-            <label for="park-filter">Parking</label>
-            <input type="checkbox" id="park-filter" name="park-filter">
-            <button class="btn btn-primary d-block mt-3" type="submit">Done</button>
+            <div class="mb-3 form-check">
+                <label class="form-check-label" for="park-filter">Parking</label>
+                <input class="form-check-input" <?php echo $parkFilter ? 'checked' : ''; ?> type="checkbox" id="park-filter" name="park-filter">
+            </div>
+            <div class="w-25">
+                <label class="mb-2">Rating Filter</label>
+                <select aria-placeholder="Rating Filter" class="form-select w-50" name="vote">
+                    <option <?php echo $voteFilter === 0 ? 'selected' : '' ?> value="0">All</option>
+                    <option <?php echo $voteFilter === 1 ? 'selected' : '' ?> value="1">At least 1</option>
+                    <option <?php echo $voteFilter === 2 ? 'selected' : '' ?> value="2">At least 2</option>
+                    <option <?php echo $voteFilter === 3 ? 'selected' : '' ?> value="3">At least 3</option>
+                    <option <?php echo $voteFilter === 4 ? 'selected' : '' ?> value="4">At least 4</option>
+                    <option <?php echo $voteFilter === 5 ? 'selected' : '' ?> value="5">5</option>
+                </select>
+            </div>
+            <button class="btn btn-primary d-block mt-4" type="submit">Done</button>
         </form>
 
         <table class="table text-center mt-5">
@@ -92,11 +115,11 @@ if ($parkFilter) {
 
             <tbody>
                 <?php foreach ($filteredHotels as $hotel) { ?>
-                        <tr>
-                            <?php foreach ($hotel as $value) { ?>
-                                <td><?php echo $value ?></td>
-                            <?php } ?>
-                        </tr>
+                    <tr>
+                        <?php foreach ($hotel as $value) { ?>
+                            <td><?php echo $value ?></td>
+                        <?php } ?>
+                    </tr>
                 <?php } ?>
             </tbody>
         </table>
